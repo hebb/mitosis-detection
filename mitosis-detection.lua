395be8996b135fcfff7d98e6b91912a44e68a414
hebb <hebb.andrew@gmail.com>
2016-01-21 05:21:06 -0500

    Added mitosis-detection.lua

---
+++ mitosis-detection.lua
@@ -0,0 +1,17 @@
+require 'torch'
+require 'nn'
+
+-- load training data
+-- train_data = load(training_set)
+
+-- define network
+-- network = define_network
+
+-- train network
+-- train(train_data, network)
+
+-- load testing data
+-- test_data = load(testing_set)
+
+-- test network
+-- results = test(test_data, network)
